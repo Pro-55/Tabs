de8e1186f034007ff0f5c012c439605d8556fdca
Pro <pranitrane619@gmail.com>
2017-04-09 15:29:41 +0530

    Initial commit

---
+++ app/src/main/java/com/pro/tabs/TabTwo.java
@@ -0,0 +1,19 @@
+package com.pro.tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabTwo extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab_two_layout, container, false);
+        return rootView;
+    }
+
+
+}
